30754823c55d0ab0d152b749a3d4063f43b6b62f
kapil kumar <kapilkumar122414@gmail.com>
2026-02-16 20:43:56 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+document.getElementById("hello").innerText="hello";
+document.getElementById("demo").innerHTML="<h2>  welcom to my college</h2>";
+
+document.writeln("how are you");
+window.alert(90);
+
+console.log(80+9);
+
+
+let a=10;
+let b=66;
+let c=a+b;
+console.log (c);
+
