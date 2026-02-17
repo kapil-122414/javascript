4e2cc258a0508bd8c64562dc00cce2390668e4d3
kapil-122414 <kapilkumar122414@gmail.com>
2026-02-17 20:35:00 +0530

    first

--- javascript.js
+++ javascript.js
@@ -19,3 +19,44 @@ let m =(x+y)*2;
 console.log(m);
 console.log(y%3);
 console.log(x=y);
+
+let t=(x>y)? "yes":"No";
+console.log(t);
+
+//switch
+let l=2;
+switch(l){
+    case 1:
+        console.log("1");
+        break;
+    case 2:
+        console.log("2");
+        break;
+    case 3:
+        console.log("3");
+        break;
+}
+
+
+console.log(10=="k");
+
+
+//Q1 
+let name="kapil";
+console.log(name);
+//Q2
+let age=25;
+console.log(age);
+//Q3
+const country="India";
+console.log(country);
+//Q4
+let count=5;
+console.log("before"+ count);
+count=10;
+console.log("after "+ count);
+//q5
+const chang=10;
+console.log(chang);
+ chang=6;
+console.log(chang);
