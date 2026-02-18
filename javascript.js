7f3edff5c4589fd5b59316f682a774a699ce81c5
kapil-122414 <kapilkumar122414@gmail.com>
2026-02-18 15:44:34 +0530

    add data

--- javascript.js
+++ javascript.js
@@ -1,62 +1,344 @@
-document.getElementById("hello").innerText="hello";
-document.getElementById("demo").innerHTML="<h2>  welcom to my college</h2>";
+// document.getElementById("hello").innerText="hello";
+// document.getElementById("demo").innerHTML="<h2>  welcom to my college</h2>";
 
-document.writeln("how are you");
-window.alert(90);
+// document.writeln("how are you");
+// window.alert(90);
 
-console.log(80+9);
-
-
-let a=10;
-let b=66;
-let c=a+b;
-console.log (c);
+// console.log(80+9);
 
 
-let x=4;
-let y=8;
-let m =(x+y)*2;
-console.log(m);
-console.log(y%3);
-console.log(x=y);
+// let a=10;
+// let b=66;
+// let c=a+b;
+// console.log (c);
 
-let t=(x>y)? "yes":"No";
-console.log(t);
 
-//switch
-let l=2;
-switch(l){
-    case 1:
-        console.log("1");
-        break;
-    case 2:
-        console.log("2");
-        break;
-    case 3:
-        console.log("3");
-        break;
+// let x=4;
+// let y=8;
+// let m =(x+y)*2;
+// console.log(m);
+// console.log(y%3);
+// console.log(x=y);
+
+// let t=(x>y)? "yes":"No";
+// console.log(t);
+
+// //switch
+// let l=2;
+// switch(l){
+//     case 1:
+//         console.log("1");
+//         break;
+//     case 2:
+//         console.log("2");
+//         break;
+//     case 3:
+//         console.log("3");
+//         break;
+// }
+
+
+// console.log(10=="k");
+
+
+// //Q1 
+// let name="kapil";
+// console.log(name);
+// //Q2
+// let age=25;
+// console.log(age);
+// //Q3
+// const country="India";
+// console.log(country);
+// //Q4
+// let count=5;
+// console.log("before"+ count);
+// count=10;
+// console.log("after "+ count);
+// //q5
+// const chang=10;
+// console.log(chang);
+//  //chang=6;
+// //console.log(chang);
+// //q6
+// {
+// let str="string"; //string
+// let number=23; //number
+// var bool=true;
+// let a;
+// const nul=null;
+
+// }
+
+// //Q7
+// {
+// let str="string";
+// console.log(typeof str);
+
+// let num=123;
+// console.log(typeof num);
+// //q8
+// let bool=true;
+// console.log(typeof bool);
+// //q8
+// let nul= null;
+// console.log(typeof nul);
+
+
+//     }
+// var scope=10;
+// {
+//     console.log( "scope in block"+scope);
+
+// }
+ 
+
+// let scops=9;
+
+
+//      function myfuncation(){
+//         console.log(" scope in function" +scope)
+//          scops=40;
+//        console.log(scops);
+//     }
+
+// console.log(scops);
+// myfuncation();
+
+// //console.log(scops);
+
+
+
+
+// // 
+// {
+//     let kapil=20;
+//     let sum=20;
+//     var sums=kapil+sum;
+   
+// }
+//  console.log(sums);
+
+
+//  //part 3
+// {
+//     console.log(typeof "hello");
+
+//     console.log(typeof 100);
+//     console.log(typeof true);
+
+//     console.log (typeof  undefined );
+//     console.log(typeof null);
+
+
+// }
+// //part 4
+// {
+// console.log(10+5);
+// console.log(10<5);
+// console.log(true&&false);
+
+// }
+
+// //level 5
+// {
+
+//     let u=5;
+//     if(u%2===0){
+//         console.log("even number");
+//     }
+//     else{
+//         console.log("odd number");
+//     }
+// }
+
+
+//Q27
+{
+    let u=16;
+    if(u>=18){
+        console.log("adult");
+
+    }
+ else{
+    console.log("minor");
+ }
+}
+
+//Q28
+// {    
+//     let a=1;
+//     if(a<0){
+//         console.log("negative");
+//     }
+//     else{
+//         console.log("postive");
+//     }
+
+// }
+
+//Q29
+// {
+//     let a=30;
+//     let b=90;
+//     if(a>b){
+//          console.log("larger a= "+ a);
+        
+//     }
+//      else if(b>a){
+//             console.log("larger b="+  b);
+//          }
+// }
+//Q30
+{
+    let a=10;
+    let b=30;
+    let c=89;
+    
 }
 
 
-console.log(10=="k");
+//Q31
+// {
+//     let a=0;
+//     if(a===0){
+//         console.log("this is 0="+ a);
+//     }
+//     else{
+//         console.log("not 0");
+//     }
 
+// }
 
-//Q1 
-let name="kapil";
-console.log(name);
-//Q2
-let age=25;
-console.log(age);
-//Q3
-const country="India";
-console.log(country);
-//Q4
-let count=5;
-console.log("before"+ count);
-count=10;
-console.log("after "+ count);
-//q5
-const chang=10;
-console.log(chang);
- chang=6;
-console.log(chang);
+//Q32
+// {
+//     let a=100;
+
+//     if(a>=90){
+//         console.log("grade "+ "+A")
+//     }
+//     else if(a>=80){
+//         console.log( "grade "+ "+B")
+//     }
+//     else if(a>=70){
+//         console.log("grade "+ "+c")
+//     }
+//     else{
+//         console.log("fail")
+//     }
+// }
+
+//Q33
+// {
+//     let a=237;
+//     if(a%5===0){
+//         console.log("divide by 5");
+//     }
+// }
+
+//Q34
+// {
+//     let a=99;
+
+//      if(a%3===0 && a%5===0){
+//         console.log("divid by 5 or 3")
+//      }
+//      else if(a%3===0){
+//         console.log("divide by 3");
+//     }
+//      else if(a%5===0){
+//         console.log("divid by 5");
+//      }
+     
+// }
+
+//Q35
+// {
+//     let a=2060;
+//     if(a%4===0){
+//         console.log("leap year");
+//     }
+
+// }
+
+//part 6
+//Q36
+// {
+//     let a=3;
+//     console.log((a<18)?"minor":"adult");
+// }
+//37
+// {
+//     let a=28;
+//     console.log((a%2===0)?"even":"odd");
+// }
+//Q38
+// {
+//     let a=20;
+//     console.log((a>=27)?"pass":"fail");
+// }
+//Q39'
+{
+    let a=90;
+}
+
+//40
+
+// {
+// let a=-20;
+// console.log((a>0)?"positive":"negative");
+// }
+
+//part 7
+//Q41
+
+// 
+//Q42
+// {
+//     let a=10;
+//     while(a>=1){
+//         console.log (a);
+//         --a;
+//     }
+
+// }
+//Q43
+// {
+//     for(let i=1;i<=20;i++){
+//         if(i%2===0){
+//             console.log("even number");
+//         }
+//     }
+// }
+//Q44
+// {
+//     for(let i=1;i<=20;i++){
+//         if(i%2!==0){
+//             console.log("odd number");
+//         }
+//     }
+// }
+//Q45
+// {
+//     for(let i=1;i<=10;i++){
+        
+//             console.log(5*i);
+        
+//     }
+// }
+//Q46
+// {
+//     let a=0;
+//      for(let i=1;i<=10;i++){
+//         a=a+i
+//            console.log(a);
+//             }
+// }
+//Q47
+{
+    let i=0;
+    while(i<=10){
+        console.log(i);
+        i++;
+    }
+}
