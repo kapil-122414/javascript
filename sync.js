10ee2608b15fe5c6793075fd7471a2c95999d636
kapil-122414 <kapilkumar122414@gmail.com>
2026-03-02 21:30:42 +0530

    sync file add

---
+++ sync.js
@@ -0,0 +1,39 @@
+{
+    // function hello(){
+    //     console.log("hello");
+        
+    // }
+    // setTimeout(hello,2000) ///2s =2000ms
+
+    // setTimeout(() => {
+    //     console.log("hello bro")
+        
+    // }, 4000);
+}
+
+//asynchronous 
+{
+// console.log("one");
+// console.log("two");
+// setTimeout(()=>{
+//     console.log("three");
+// }, 20);
+// console.log("four");
+// console.log("five");
+// // setTimeout(()=>{
+// // console.log("four");
+// // console.log("five");
+// // },0)
+
+}
+//callbacks
+{
+    function sum(a,b){
+        return a+b;
+    }
+    function callback(a ,b ,callbacks){
+    callbacks(a,b);
+    }
+   let sums= callback(1,2 ,sum)
+console.log(sums);
+}
