812d949290f58d0a270723d0e9d3d3dab6653e2e
kapil-122414 <kapilkumar122414@gmail.com>
2026-03-03 21:03:11 +0530

    add the promise

--- sync.js
+++ sync.js
@@ -28,12 +28,108 @@
 }
 //callbacks
 {
-    function sum(a,b){
-        console.log(a+b);
-    }
-    function callback(a ,b ,callbacks){
-    callbacks(a,b);
-    }
-    callback(1,2 ,sum)
+    // function sum(a,b){
+    //     console.log(a+b);
+    // }
+    // function callback(a ,b ,callbacks){
+    // callbacks(a,b);
+    // }
+    // callback(1,2 ,sum)
 
+}
+//callback hell
+{
+
+// function callback(a ,getdata){
+//     setTimeout(()=>{
+// console.log(a);
+// if(getdata){
+// getdata();
+// }
+//     },2000);
+
+
+// }
+// callback(1,()=>{
+//     console.log("running 2 ");
+//   callback(2,()=>{
+//     console.log("running 3");
+//     callback(3,()=>{
+//         console.log("running 4");k
+//         callback(4)
+//     });
+//   });
+// });
+
+}
+
+
+{
+    // function setdta(x){
+    //     setTimeout(() => {
+    //         console.log(x);
+    //     }, 4000);
+    // }
+    // setdta(1);
+    // setdta(2);
+    // setdta(3);
+}
+
+//promises
+{
+//     let promises=new Promise((resolve ,reject)=>{
+// console.log("i promises");
+// resolve("successfuly");
+
+//     })
+//     console.log(promises);
+
+// function getdata(data){
+//     return  new Promise((resolve,reject)=>{
+//       setTimeout(()=>{
+//         console.log("i promise",data);
+//         resolve("sucess");
+//       },5000)
+//     })
+// }
+// console.log(getdata(12))
+} 
+//promise.then 
+{
+//     const getpromise=()=>{
+// return new Promise((resolve ,reject)=>{
+// console.log("i promises");
+// //  resolve("successfuly");
+// reject("404 error");
+
+//     })
+//     };
+
+//     let promise=getpromise();
+
+//  promise.then((res)=>{
+//     console.log("fulfiled",res);
+//  })
+//  promise.catch((err)=>{
+//     console.log("reject ",err);
+
+//  })
+    
+}
+
+{
+      const   getdata=()=>{
+        return new Promise((resolve,reject)=>{
+            console.log("i am promise");
+            resolve("successfully");
+        });
+
+    }
+
+    let promises=getdata();
+    promises.then((res)=>{
+        console.log(" data insert",res);
+    });
+
+    
 }
